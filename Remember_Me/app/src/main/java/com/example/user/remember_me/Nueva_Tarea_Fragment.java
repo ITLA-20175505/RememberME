1c7e64a4884e1196b16772b54ae46b06be8b8c54
Freddysotof <freddy.sotof23@hotmail.com>
2019-04-08 23:19:22 -0400

    Calendario y Reloj

--- Remember_Me/app/src/main/java/com/example/user/remember_me/Nueva_Tarea_Fragment.java
+++ Remember_Me/app/src/main/java/com/example/user/remember_me/Nueva_Tarea_Fragment.java
@@ -12,9 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.example.user.remember_me.Coordinador.CoordinadorTask;
+import com.example.user.remember_me.Logica.LogicaTask;
 
 import java.util.Calendar;
 
@@ -22,9 +26,12 @@ import java.util.Calendar;
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class Nueva_Tarea_Fragment extends Fragment {
 
-    Spinner nt_spinner;
-    Spinner est_spinner;
-
+    private Spinner mnt_spinner;
+    private Spinner mest_spinner;
+    private EditText mtxtNombreTarea;
+    private EditText mtxtNota;
+    private CoordinadorTask mcoordTask;
+    private LogicaTask mlogicaTask
     @SuppressLint("WrongViewCast")
     @Nullable
     @Override
@@ -33,23 +40,25 @@ public class Nueva_Tarea_Fragment extends Fragment {
         View view;
         view = inflater.inflate(R.layout.fragment_nueva__tarea_, container, false);
 
+        mtxtNombreTarea = (EditText) view.findViewById(R.id.nom_tarea);
+        mtxtNota = (EditText) view.findViewById(R.id.txt_nota);
 
 
         //Llenando el nt_spinner del array en el archivo strings
         String[] frecuencia;
-        nt_spinner = (Spinner) view.findViewById(R.id.nt_spinner);
+        mnt_spinner = (Spinner) view.findViewById(R.id.nt_spinner);
         frecuencia = getResources().getStringArray(R.array.nt_spinner_array);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, frecuencia);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        nt_spinner.setAdapter(arrayAdapter);
+        mnt_spinner.setAdapter(arrayAdapter);
 
         //Llenando el est_spinner del array en el archivo strings
         String[] estandar;
-        est_spinner = (Spinner) view.findViewById(R.id.est_spinner);
+        mest_spinner = (Spinner) view.findViewById(R.id.est_spinner);
         estandar = getResources().getStringArray(R.array.est_spinner_array);
         ArrayAdapter<String> array_Adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, estandar);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        est_spinner.setAdapter(array_Adapter);
+        mest_spinner.setAdapter(array_Adapter);
 
 
 
---
+++ Remember_Me/app/src/main/java/com/example/user/remember_me/calendarView.java
@@ -0,0 +1,13 @@
+package com.example.user.remember_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class calendarView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_view);
+    }
+}
